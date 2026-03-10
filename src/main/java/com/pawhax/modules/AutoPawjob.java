d09ecc013e1fccafb8a448c9a4a1748bd2c4d94a
Olstar <olstar@olstar123.net>
2026-03-10 00:22:36 +0000

    autopawjob better now
    Added a shoutout comment in the AutoPawjob class.

--- src/main/java/com/pawhax/modules/AutoPawjob.java
+++ src/main/java/com/pawhax/modules/AutoPawjob.java
@@ -3,6 +3,10 @@ package com.pawhax.modules;
 import com.pawhax.PawHax;
 import meteordevelopment.meteorclient.systems.modules.Module;
 
+/*
+shoutout dekto, chatgpt, e621, the pawbots, 87ac, and my goat bleekea <3
+*/
+
 public class AutoPawjob extends Module {
     public AutoPawjob() {
         super(PawHax.CATEGORY, "AutoPawjob", "Automatically gives pawjobs :3");
