cc21087f173dbf095beda8a4641a4178d8e01dfa
CrisisSheep <41985051+CrisisSheep@users.noreply.github.com>
2026-05-25 03:04:55 +1200

    add AutoTorch module

--- src/main/java/com/pawhax/PawHax.java
+++ src/main/java/com/pawhax/PawHax.java
@@ -37,6 +37,7 @@ public class PawHax extends MeteorAddon {
         Modules.get().add(new DiagBounce());
         Modules.get().add(new BannerWebhook());
         Modules.get().add(new NowPlayingModule());
+        Modules.get().add(new AutoTorch());
 
         LOG.info("imported larp modules........");
         LOG.info("rat successfully installed! :3");
